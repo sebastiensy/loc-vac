a3428c45bc994678f6e66f362de2b1368b998cbe
Julien WAZAN <julien.wazan@gmail.com>
2015-10-06 13:34:16 +0200

    Location bdd

---
+++ loc-vac/src/main/java/fr/uha/miage/locvac/model/Location.java
@@ -0,0 +1,85 @@
+package fr.uha.miage.locvac.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Location {
+	
+	@Id
+	@GeneratedValue
+	private int idLocation;
+	
+	private String nomLocation;
+	private int capaciteLocation;
+	private String villeLocation;
+	private String typeProprieteLocation;
+	private String paysLocation;
+	private String adresseLocation;
+	private int codePostalLocation;
+	private double prixLocation;
+	private String descriptionLocation;
+	
+	
+	
+	public int getIdLocation() {
+		return idLocation;
+	}
+	public void setIdLocation(int idLocation) {
+		this.idLocation = idLocation;
+	}
+	public String getNomLocation() {
+		return nomLocation;
+	}
+	public void setNomLocation(String nomLocation) {
+		this.nomLocation = nomLocation;
+	}
+	public int getCapaciteLocation() {
+		return capaciteLocation;
+	}
+	public void setCapaciteLocation(int capaciteLocation) {
+		this.capaciteLocation = capaciteLocation;
+	}
+	public String getVilleLocation() {
+		return villeLocation;
+	}
+	public void setVilleLocation(String villeLocation) {
+		this.villeLocation = villeLocation;
+	}
+	public String getTypeProprieteLocation() {
+		return typeProprieteLocation;
+	}
+	public void setTypeProprieteLocation(String typeProprieteLocation) {
+		this.typeProprieteLocation = typeProprieteLocation;
+	}
+	public String getPaysLocation() {
+		return paysLocation;
+	}
+	public void setPaysLocation(String paysLocation) {
+		this.paysLocation = paysLocation;
+	}
+	public String getAdresseLocation() {
+		return adresseLocation;
+	}
+	public void setAdresseLocation(String adresseLocation) {
+		this.adresseLocation = adresseLocation;
+	}
+	public int getCodePostalLocation() {
+		return codePostalLocation;
+	}
+	public void setCodePostalLocation(int codePostalLocation) {
+		this.codePostalLocation = codePostalLocation;
+	}
+	public double getPrixLocation() {
+		return prixLocation;
+	}
+	public void setPrixLocation(double prixLocation) {
+		this.prixLocation = prixLocation;
+	}
+	public String getDescriptionLocation() {
+		return descriptionLocation;
+	}
+	public void setDescriptionLocation(String descriptionLocation) {
+		this.descriptionLocation = descriptionLocation;
+	}
+	
+}
---
+++ loc-vac/src/main/java/fr/uha/miage/locvac/repository/LocationRepository.java
@@ -0,0 +1,9 @@
+package fr.uha.miage.locvac.repository;
+import org.springframework.data.repository.CrudRepository;
+
+import fr.uha.miage.locvac.model.Location;
+
+public interface LocationRepository extends CrudRepository<Location, Integer>{
+
+	
+}
