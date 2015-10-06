a9b79749e4193b82e54272ce58f2dc7ce595ab7e
sebastiensy <seb6890@gmail.com>
2015-10-06 13:34:25 +0200

    Creation d'un controller pour l'inscription

---
+++ loc-vac/src/main/java/fr/uha/miage/locvac/controller/InscriptionController.java
@@ -0,0 +1,27 @@
+package fr.uha.miage.locvac.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fr.uha.miage.locvac.model.User;
+
+@Controller
+public class InscriptionController {
+	
+	@RequestMapping(value="/inscription", method=RequestMethod.GET)
+    public String affichageFormInscription(Model model) {
+    	model.addAttribute("user", new User());
+    	return "inscription";
+    }
+    
+    @RequestMapping(value="/inscription", method=RequestMethod.POST)
+    public String formInscriptionSubmit(@ModelAttribute User user, Model model) {
+    	model.addAttribute("user", user);
+    	return "inscription";
+    }
+    
+
+}
--- loc-vac/src/main/java/fr/uha/miage/locvac/controller/WebController.java
+++ loc-vac/src/main/java/fr/uha/miage/locvac/controller/WebController.java
@@ -2,7 +2,9 @@ package fr.uha.miage.locvac.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.uha.miage.locvac.model.User;
@@ -24,12 +26,6 @@ public class WebController {
     @RequestMapping("/index")
     public String index() {
     	return "index";
-    }
-    
-    @RequestMapping("/inscription")
-    public String affichageForminscription(Model model) {
-    	model.addAttribute("user", new User());
-    	return "inscription";
     }
     
     @RequestMapping("/destinations")
