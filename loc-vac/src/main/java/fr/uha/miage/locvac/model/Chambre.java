902fe1182e6f835991587b80ef05326d49ab8511
Julien WAZAN <julien.wazan@gmail.com>
2015-10-06 14:17:39 +0200

    maj chambre

--- loc-vac/src/main/java/fr/uha/miage/locvac/model/Chambre.java
+++ loc-vac/src/main/java/fr/uha/miage/locvac/model/Chambre.java
@@ -1,10 +1,14 @@
 package fr.uha.miage.locvac.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Chambre {
 	
+	@Id
+	@GeneratedValue
 	private int idChambre;
 
 	public int getIdChambre() {
