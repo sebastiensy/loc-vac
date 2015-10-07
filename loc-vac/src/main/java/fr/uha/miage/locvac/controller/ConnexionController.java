a9cab3316e35e7a4e7078769e1c1e2a90f4cbaf7
sebastiensy <seb6890@gmail.com>
2015-10-07 10:21:00 +0200

    Page de connexion

---
+++ loc-vac/src/main/java/fr/uha/miage/locvac/controller/ConnexionController.java
@@ -0,0 +1,35 @@
+package fr.uha.miage.locvac.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fr.uha.miage.locvac.model.User;
+import fr.uha.miage.locvac.repository.UserRepository;
+
+@Controller
+public class ConnexionController {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	// pour afficher le formulaire de connexion
+	@RequestMapping(value="/connexion", method=RequestMethod.GET)
+	public String affichageFormConnexion(HttpSession session, Model model) {
+		model.addAttribute("user", new User());
+		return "connexion";
+	}
+
+
+	// bouton submit du formulaire
+	@RequestMapping(value="/connexion", method=RequestMethod.POST)
+	public String formInscriptionSubmit(HttpSession session, @ModelAttribute User user, Model model) {
+		model.addAttribute("user", user);
+		return "connexion";
+	}
+}
