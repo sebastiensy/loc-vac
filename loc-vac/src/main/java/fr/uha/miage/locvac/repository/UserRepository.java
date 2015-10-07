fcd7aeafe77e9fb8e9e06584def18963d4d3e836
sebastiensy <seb6890@gmail.com>
2015-10-07 15:45:27 +0200

    Modification de userrepository

--- loc-vac/src/main/java/fr/uha/miage/locvac/repository/UserRepository.java
+++ loc-vac/src/main/java/fr/uha/miage/locvac/repository/UserRepository.java
@@ -6,4 +6,5 @@ import fr.uha.miage.locvac.model.User;
 
 public interface UserRepository extends CrudRepository<User, Integer>{
 
+	User findByEmailUserAndMdpUser(String emailUser, String mdpUser);
 }
