8ff8c0220d5bc33e465977aff3570a16fdcb650d
sebastiensy <seb6890@gmail.com>
2015-10-09 11:55:49 +0200

    Equipement en cours

--- loc-vac/src/main/java/fr/uha/miage/locvac/controller/AdminController.java
+++ loc-vac/src/main/java/fr/uha/miage/locvac/controller/AdminController.java
@@ -1,7 +1,16 @@
 package fr.uha.miage.locvac.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fr.uha.miage.locvac.model.Equipement;
 
 @Controller
 public class AdminController {
@@ -31,6 +40,24 @@ public class AdminController {
 	@RequestMapping("/admin/afficherlocations")
     public String afficheLocations() {
         return "/admin/afficherlocations";
+    }
+	
+	@RequestMapping(value="/admin/equipements", method=RequestMethod.GET)
+    public String afficheFormEquipement(HttpSession session, Equipement equipement) {
+        return "/admin/equipements";
+    }
+	
+	@RequestMapping(value="/admin/equipements", method=RequestMethod.POST)
+    public String afficheEquipement(HttpSession session, Equipement equipement, BindingResult bindingResult) {
+		List<Equipement> cart = (List<Equipement>) session.getAttribute("cart");
+    	if (cart == null)
+     		cart = new ArrayList<Equipement>();
+    	
+    	if (!bindingResult.hasErrors())
+    		cart.add(equipement);
+   	
+		session.setAttribute("cart", cart);
+		return "/admin/equipements";
     }
 
 }
